251e7a2a11e0fbdd87bd104d1ecfc0aaca29cf47
TristanMacaulay <tristanmacaulay6@gmail.com>
2020-06-12 22:40:50 +1000

    making final adjustment including getting rid off unnessesary code

---
+++ program/app/app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+    def contact_us
+
+    end  
+end
--- program/app/config/routes.rb
+++ program/app/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :resources
-  resources :cities do
-    member do
-      post :create_via_search
-    end
-  end
-  resources :zones
+  #resources :resources
+  #resources :zones
+  resources :cities
+
   
   root 'cities#index'
+  get '/contact_us', to: 'static_pages#contact_us'
+  #get 'static_pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
